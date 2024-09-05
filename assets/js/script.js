3ec9fece1ab62350d12eb26ae00d5738c3040fd1
Nathan Steele <9lf7wmbn@students.codeinstitute.net>
2024-09-05 23:10:36 +0000

    started work on adding JS code by defining vairable and adding placeholder functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,52 @@
-let quizArea = document.getElementsByClassName('quiz-area');
-let score = document.getElementById('score');
-let submit = document.getElementsByClassName('btn-submit-answer');
-
-function build() {
-
-}
-
-function generateScore() {
-
-}
-build();
-submit.addEventListener('click', generateScore);
-let quiz = [
+const gameRules = document.getElementById("how-to-play");
+const startBtn = document.getElementById("btn-start");
+const nextButton = document.getElementById("btn-submit-answer");
+const quizAreaElement = document.getElementById('quiz-area');
+const questionElement = document.getElementsByClassName('question');
+const potentialAnswer = document.getElementsByClassName('potential-answers');
+let Score = 0;
+let questionNumber = 0;
+const questions = [
     {
-        question: "In what year was the film 'Star Wars' released in theaters?",
-        potentialAnswers: ["1981", "1972", "1977", "1989"],
-        answer: "1977"
+        question: "In what year was the movie 'Star Wars' relased?",
+        option: '1979',
+        option: '1987',
+        option: '1981',
+        option: '1977', 
+        answer: '1977'
     }, 
     {
-        question: "Who was the first character or group to have thier own comic book in Marvel Comics?",
-        potentialAnswers: ["Natasha Romanov", "Fantastic Four", "Matt Murdoch", "X-Men"],
-        answers: "Fantastic Four"
-    }, 
-    {
-        question: "Season One of 'Star Trek: The Original Series' was first released in what year?",
-        potentialAnswers: ["1946", "1982", "1966", "1971"],
-        answer: "1966"
-    }, 
-    {
-        question: "Who directed the famous 1979 film 'Alien?'",
-        potentialAnswers: ["David Lynch", "Martin Scorsese", "Ridley Scott", "John Hughes"],
-        answer: "Ridley Scott"
+        question: "In what year did Marvel Comics pulish the first 'Fantastic Four' Comic?",
+        option: '1961',
+        option: '1949',
+        option: '1954',
+        option: '1972',
+        answer: '1961'
     }
 ]
+
+startBtn.addEventListener('click', playGame);
+nextButton.addEventListener('click', nextQuestion);
+
+    
+
+function playGame() {
+    score = 0;
+    questionNumber = 0;
+    console.log('Playing Game!');
+    gameRules.classList.add("hide");
+    startBtn.classList.add("hide");
+    quizAreaElement.classList.remove("hide");
+    nextQuestion();
+}
+
+function nextQuestion() {
+    questionNumber++;
+    console.log(questionNumber);
+    displayQuestion()
+}
+
+function displayQuestion() {
+}
+
+displayQuestion();
