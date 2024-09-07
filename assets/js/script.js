ab5d3cee2ad8c5ec3c811f2ed3f9b3769f020394
Nathan Steele <9lf7wmbn@students.codeinstitute.net>
2024-09-07 17:26:06 +0000

    added to js file to make questions and answer options appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,69 @@
-const gameRules = document.getElementById("how-to-play");
-const startBtn = document.getElementById("btn-start");
+
 const nextButton = document.getElementById("btn-submit-answer");
-const quizAreaElement = document.getElementById('quiz-area');
-const questionElement = document.getElementsByClassName('question');
-const potentialAnswer = document.getElementsByClassName('potential-answers');
-let Score = 0;
-let questionNumber = 0;
-const questions = [
+let score = 0;
+
+const myQuestionsArray = [
     {
         question: "In what year was the movie 'Star Wars' relased?",
-        option: '1979',
-        option: '1987',
-        option: '1981',
-        option: '1977', 
+        optionOne: '1979',
+        optionTwo: '1987',
+        optionThree: '1981',
+        optionFour: '1977', 
         answer: '1977'
     }, 
     {
-        question: "In what year did Marvel Comics pulish the first 'Fantastic Four' Comic?",
-        option: '1961',
-        option: '1949',
-        option: '1954',
-        option: '1972',
+        question: "In what year did Marvel Comics publish the first 'Fantastic Four' Comic?",
+        optionOne: '1961',
+        optionTwo: '1949',
+        optionThree: '1954',
+        optionFour: '1972',
         answer: '1961'
     }
 ]
+const startBtn = document.getElementById("btn-start");
 
 startBtn.addEventListener('click', playGame);
 nextButton.addEventListener('click', nextQuestion);
 
     
+const gameRules = document.getElementById("how-to-play");
+const quizAreaElement = document.getElementById('quiz-area');
 
 function playGame() {
     score = 0;
-    questionNumber = 0;
+    questionNumberIndex = 0;
     console.log('Playing Game!');
     gameRules.classList.add("hide");
     startBtn.classList.add("hide");
     quizAreaElement.classList.remove("hide");
-    nextQuestion();
+    displayQuestion();
 }
 
 function nextQuestion() {
-    questionNumber++;
-    console.log(questionNumber);
-    displayQuestion()
+    displayQuestion();
 }
+const potentialAnswerOne = document.getElementById('potential-answer-one');
+const potentialAnswerTwo = document.getElementById('potential-answer-two');
+const potentialAnswerThree = document.getElementById('potential-answer-three');
+const potentialAnswerFour = document.getElementById('potential-answer-four');
+const questionElement = document.getElementById("question");
+let questionNumber = document.getElementById("question-number")
 
 function displayQuestion() {
-}
+    questionNumber.innerHTML = questionNumberIndex + 1;
 
+    const index = Math.floor(Math.random() * myQuestionsArray.length)
+    const question = myQuestionsArray[index]
+    questionElement.innerHTML = question.question;
+    potentialAnswerOne.innerHTML = question.optionOne
+    potentialAnswerTwo.innerHTML = question.optionTwo
+    potentialAnswerThree.innerHTML = question.optionThree
+    potentialAnswerFour.innerHTML = question.optionFour
+
+    if (option == correct) {
+        option.document.body.style.color = "#bcd8c1" 
+    } else {
+        option.document.body.style.color = "#E87070" 
+    }
+}
 displayQuestion();
