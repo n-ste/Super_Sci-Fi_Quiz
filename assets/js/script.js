71c110c6a78d7c15a4869121f7fc6a89beff3b94
Nathan Steele <9lf7wmbn@students.codeinstitute.net>
2024-09-08 00:34:23 +0000

    added and removed code due to errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,78 @@
 const nextButton = document.getElementById("btn-submit-answer");
 let userScore = document.getElementById("score")
+let questionNumberIndex = 1;
 
 const myQuestionsArray = [{
         question: "In what year was the movie 'Star Wars' relased?",
-        optionOne: '1979',
-        optionTwo: '1987',
-        optionThree: '1981',
-        optionFour: '1977',
-        answer: '1977'
+        optionOne: '1979', correct: false,
+        optionTwo: '1987', correct: false,
+        optionThree: '1981', correct: false,
+        optionFour: '1977', correct: true
     },
     {
         question: "In what year did Marvel Comics publish the first 'Fantastic Four' Comic?",
-        optionOne: '1961',
-        optionTwo: '1949',
-        optionThree: '1954',
-        optionFour: '1972',
-        answer: '1961'
+        optionOne: '1961', correct: true,
+        optionTwo: '1949', correct: false,
+        optionThree: '1954', correct: false,
+        optionFour: '1972', correct: false
+    
     },
     {
         question: "What is the Doctor’s home planet in ‘Doctor Who’?",
-        optionOne: 'Auros',
-        optionTwo: 'Tattooine',
-        optionThree: 'Anagonia',
-        optionFour: 'Gallifrey',
-        answer: 'Gallifrey'
+        optionOne: 'Auros', correct: false,
+        optionTwo: 'Tattooine', correct: false,
+        optionThree: 'Anagonia', correct: false,
+        optionFour: 'Gallifrey', correct: true
     },
     {
         question: "Where we’re going, we don’t need roads” is a quote from which 80’s time travel movie?",
-        optionOne: 'Star Wars: Return of the Jedi',
-        optionTwo: 'Back to the Future II',
-        optionThree: 'The Terminatior',
-        optionFour: 'Twelve Monkeys',
-        answer: 'Back to the Future II'
+        optionOne: 'Star Wars: Return of the Jedi', correct: false,
+        optionTwo: 'Back to the Future II', correct: false, 
+        optionThree: 'The Terminatior', correct: false,
+        optionFour: 'Twelve Monkeys', correct: false,
+        answer: 'Back to the Future II', correct: true
     },
     {
         question: "Who directed the 1979 movie Alien?",
-        optionOne: 'Martin Scorsese',
-        optionTwo: 'John Hughes',
-        optionThree: 'Ridley Scott',
-        optionFour: 'George Lucas',
-        answer: 'Ridley Scott'
+        optionOne: 'Martin Scorsese', correct: false,
+        optionTwo: 'John Hughes', correct: false,
+        optionThree: 'Ridley Scott', correct: true,
+        optionFour: 'George Lucas', correct: false
     },
     {
         question: "Who trained Yoda to use his Jedi powers?",
-        optionOne: 'Anakin Skywalker',
-        optionTwo: 'Darth Sidious',
-        optionThree: 'Qui Gon Jinn',
-        optionFour: 'N’Kata Del Gormo',
-        answer: 'N’Kata Del Gormo'
+        optionOne: 'Anakin Skywalker', correct: false,
+        optionTwo: 'Darth Sidious', correct: false,
+        optionThree: 'Qui Gon Jinn', correct: false,
+        optionFour: 'N’Kata Del Gormo', correct: true
     },
     {
         question: "Ellie and Joel are the main characters in which post-apocalyptic video game turned TV series?",
-        optionOne: 'The Matrix',
-        optionTwo: 'Star Wars: Battlefront',
-        optionThree: 'The Last of Us',
-        optionFour: 'The Sims',
-        answer: 'The Last of Us'
+        optionOne: 'The Matrix', correct: false,
+        optionTwo: 'Star Wars: Battlefront', correct: false,
+        optionThree: 'The Last of Us', correct: true,
+        optionFour: 'The Sims', correct: false
     },
     {
         question: "What planet is ‘Dune’ set on?",
-        optionOne: 'Mars',
-        optionTwo: 'Exegol',
-        optionThree: 'Arrakis',
-        optionFour: 'Naboo',
-        answer: 'Arrakis'
+        optionOne: 'Mars', correct: false,
+        optionTwo: 'Exegol', correct: false,
+        optionThree: 'Arrakis', correct: true,
+        optionFour: 'Naboo', correct: false
     },
     {
         question: "Who plays Spock in the original series of ‘Star Trek’?",
-        optionOne: 'William Shatner',
-        optionTwo: 'Zachary Quinto',
-        optionThree: 'Leonard Nimoy',
-        optionFour: 'DeForest Kelley',
-        answer: 'Leonard Nimoy'
+        optionOne: 'William Shatner', correct: false,
+        optionTwo: 'Zachary Quinto', correct: false,
+        optionThree: 'Leonard Nimoy', correct: true,
+        optionFour: 'DeForest Kelley', correct: false
     },
     {
         question: "In what decase is the TV show Stranger Things set in?",
-        optionOne: '2000s',
-        optionTwo: '1980s',
-        optionThree: '1990s',
-        optionFour: '1970s',
-        answer: '1980s'
+        optionOne: '2000s', correct: false,
+        optionTwo: '1980s', correct: true,
+        optionThree: '1990s', correct: false,
+        optionFour: '1970s', correct: false
     }
 ]
 const startBtn = document.getElementById("btn-start");
@@ -100,6 +93,7 @@ function playGame() {
     displayQuestion();
 }
 
+
 function nextQuestion() {
     displayQuestion();
 }
@@ -107,20 +101,24 @@ const potentialAnswerOne = document.getElementById('potential-answer-one');
 const potentialAnswerTwo = document.getElementById('potential-answer-two');
 const potentialAnswerThree = document.getElementById('potential-answer-three');
 const potentialAnswerFour = document.getElementById('potential-answer-four');
+const potentialAnswer = document.getElementById("potential-answer")
 const questionElement = document.getElementById("question");
 let questionNumber = document.getElementById("question-number")
-let questionNumberIndex = 0;
+
 
 function displayQuestion() {
-    questionNumber.innerHTML = questionNumberIndex + 1;
-    const index = Math.floor(Math.random() * myQuestionsArray.length)
-    const question = myQuestionsArray[index]
+const index = Math.floor(Math.random() * myQuestionsArray.length)
+const question = myQuestionsArray[index]
+questionNumber.innerHTML = questionNumberIndex++;
+questionElement.innerHTML = question.question;
 
-    questionElement.innerHTML = question.question;
-    potentialAnswerOne.innerHTML = question.optionOne
-    potentialAnswerTwo.innerHTML = question.optionTwo
-    potentialAnswerThree.innerHTML = question.optionThree
-    potentialAnswerFour.innerHTML = question.optionFour
+
+potentialAnswerOne.innerHTML = question.optionOne;
+potentialAnswerTwo.innerHTML = question.optionTwo;
+potentialAnswerThree.innerHTML = question.optionThree;
+potentialAnswerFour.innerHTML = question.optionFour;
+
+}
 
     /* if (option == correct) {
          option.document.body.style.color = "#bcd8c1" 
@@ -129,5 +127,6 @@ function displayQuestion() {
          option.document.body.style.color = "#E87070" 
          alert("Answer Incorrect!")
      }*/
-}
-displayQuestion();
+
+
+
