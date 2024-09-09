6a623abbcd61c2ff9d9aa3cd1988672ef5d19de7
Nathan Steele <9lf7wmbn@students.codeinstitute.net>
2024-09-09 00:00:18 +0000

    questions added to js array and functions added to display questions and answers on quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let myQuestionArray = [
-        {
+    let userScore = document.getElementById("score");
+    let myQuestionArray = [{
             question: "In what year was the movie 'Star Wars' relased?",
             options: ["1979", "1987", "1981", "1977"],
             correct: "1977",
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         {
             question: "Where we’re going, we don’t need roads” is a quote from which 80’s time travel movie?",
-            options: ["Star Wars: Return of the Jedi", "Back to the Future II", "The Terminatior", "Twelve Monkeys", "Back to the Future II"],
+            options: ["Star Wars: Return of the Jedi", "Back to the Future II", "The Terminatior", "Twelve Monkeys"],
             correct: "Back to the Future II",
         },
         {
@@ -32,56 +32,91 @@ document.addEventListener("DOMContentLoaded", () => {
         },
         {
             question: "Ellie and Joel are the main characters in which post-apocalyptic video game turned TV series?",
-            options: ["The Matrix","Star Wars: Battlefront","The Last of Us","The Sims"],
+            options: ["The Matrix", "Star Wars: Battlefront", "The Last of Us", "The Sims"],
             correct: "The Last of Us",
         },
         {
             question: "What planet is ‘Dune’ set on?",
-            options: ["Mars", "Exegol","Arrakis","Dormir"],
+            options: ["Mars", "Exegol", "Arrakis", "Dormir"],
             correct: "Arrakis",
         },
         {
             question: "Who plays Spock in the original series of ‘Star Trek’?",
-            options: ["William Shatner","Zachary Quinto","Leonard Nimoy","DeForest Kelley"],
+            options: ["William Shatner", "Zachary Quinto", "Leonard Nimoy", "DeForest Kelley"],
             correct: "Leonard Nimoy",
         },
         {
             question: "In what decase is the TV show Stranger Things set in?",
             options: ["2000s", "1980s", "1990s", "1970s"],
             correct: "1980s"
-    
+
         }
     ];
 
-    const questionArea = document.getElementById('question-area');
+    const quizArea = document.getElementById('quiz-area');
     const question = document.getElementById("question");
     const options = document.getElementById("answers");
     const submitButton = document.querySelector(".submit-buttons .btn-submit-answer");
     let questionNumber = document.getElementById("question-number");
+    const endQuiz = document.querySelector("quiz-ends");
     let questionNo = 0;
+    let currentUserScore = 0;
 
+    function scoreIncrement() {
+        userScore.innerHTML = `${currentUserScore}`;
+    };
+
+    const verifyAnswer = (e) => {
+        let selectedAnswer = e.target.textContent;
+        console.log(selectedAnswer);
+        if (selectedAnswer == myQuestionArray[questionNo].correct) {
+            currentUserScore + 1;
+            e.target.classList.add("correct");
+        } else {
+            e.target.classList.add("incorrect");
+        }
+        let disablePointer = document.querySelectorAll(".question-container .answer-option");
+        disablePointer.forEach((option) => {
+            option.classList.add = "disabled";
+        });
+    };
+    const questionAllowance = 10;
+    const questionShuffle = (array) => {
+        return array.slice().sort(() => Math.random() - 0.5);
+    };
+
+    myQuestionArray = questionShuffle(myQuestionArray);
     // Help displaying the questions and options for quiz sources from Youtube, credits in README file.
     const displayQuestions = () => {
+        options.innerHTML = "";
         questionNumber.innerHTML = questionNo + 1;
         question.innerHTML = myQuestionArray[questionNo].question;
 
         myQuestionArray[questionNo].options.forEach((option) => {
-            const AnswerOption = document.createElement("button");
-            AnswerOption.classList.add("answer-option");
-            AnswerOption.innerHTML = option;
-            options.appendChild(AnswerOption);
+            const answerOption = document.createElement("button");
+            answerOption.classList.add("answer-option");
+            answerOption.innerHTML = option;
+            answerOption.addEventListener("click", (e) => {
+                verifyAnswer(e);
+            })
+            options.appendChild(answerOption);
         });
     }
-    function questionIncrement() {
-        questionNo++;
-        questionNumber++;
-    }
+
+    displayQuestions();
 
     function nextQuestion() {
-        submitButton.addEventListener("click", questionIncrement);
+        if (questionNo >= questionAllowance - 1) {
+            return;
+        } 
+        
+        questionNo++;
+        displayQuestions();
     };
-    displayQuestions();
-    nextQuestion();
-    questionIncrement();
+    function quizComplete() {
+        quizArea.classList.add("hide");
+    }
+    quizComplete();
+    submitButton.addEventListener("click", nextQuestion);
+    scoreIncrement();
 });
-    
